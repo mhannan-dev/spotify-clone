f823644b28f6642ea5caed8f97930c0e4f5312a9
MHannan <mdhannan.info@gmail.com>
2022-02-07 13:59:44 +0600

    working on reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import Login from "./parts/Login";
+import { React, useEffect, useState } from "react"
+import Login from "./parts/Login"
+import { getTokenFromUrl } from './config/spotify'
+import SpotifyWebApi from "spotify-web-api-js"
+const spotify = new SpotifyWebApi()
 function App() {
+  const [token, setToken] = useState(null)
+  //Similar to componentDidMount and componentDidUpdate:
+  //Run based on a give condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    //Token  
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getMe().then((user)=>{
+        console.log('User', user);
+      })
+    }
+    //console.log('This is token', token);
+  }, []);
   return (
     <div className="app">
-      <h1>Welcome to Spotify Clone Using React JS</h1>
-      <Login/>
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
-
 export default App;
